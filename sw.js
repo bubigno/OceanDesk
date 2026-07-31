8608d7d86d0ab4a976bbd6d3b88ee99939c86f71
bubigno <burastero13@gmail.com>
2026-07-30 22:41:02 -0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'day-counter-v4';
+const CACHE_NAME = 'day-counter-v5';
 const urlsToCache = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './timone.png'];
 
 self.addEventListener('install', (event) => {
